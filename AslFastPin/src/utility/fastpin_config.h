ccc51fcb1d95ca4bf08fe0105fdf41c6aabd77b5
Julian Sanin <sanin89julian@gmail.com>
2017-04-04 12:57:33 +0200

    Updated internal libraries

---
+++ AslFastPin/src/utility/fastpin_config.h
@@ -0,0 +1,17 @@
+#ifndef __INC_FASTPIN_CONFIG_H
+#define __INC_FASTPIN_CONFIG_H
+
+#include "AslFastPin.h"
+
+///@file fastpin_config.h
+/// Contains definitions that can be used to configure FastPin at compile time.
+
+// Use this option only for debugging pin access and forcing software pin access.  Note that
+// software pin access only works in Arduino based environments.  Forces use of digitalWrite
+// methods for pin access vs. direct hardware port access
+// #define FASTPIN_FORCE_SOFTWARE_PINS
+
+#endif
+
+
+#endif // __INC_FASTPIN_CONFIG_H
